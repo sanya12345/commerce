62049af5748bda218c8afb96df20f707556df17c
sanya12345 <sanya.17bec1170@abes.ac.in>
2019-12-06 22:33:22 +0530

    Side navigation

--- app/src/main/java/com/coding/e_commerce/MainActivity.java
+++ app/src/main/java/com/coding/e_commerce/MainActivity.java
@@ -1,5 +1,6 @@
 package com.coding.e_commerce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent  intent = new Intent(this,homenav.class);
+        startActivity(intent);
     }
+
 }
